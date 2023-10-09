028d942a82468b5546db7d08eab62f4206956a10
MBilal233 <bilalameer233@gmail.com>
2023-10-09 17:25:16 +0500

    Update Authentication.js

--- Authentication.js
+++ Authentication.js
@@ -8,10 +8,10 @@ console.log(path);
 if (!userData && window.location.pathname === "/ForTesting/" || window.location.pathname === "/ForTesting/index.html" ) {
   window.location.assign("/ForTesting/register.html");
 }
-if (userData && window.location.pathname === "/ForTesting/register.html") {
-  window.location.assign("/ForTesting/")
-  console.log('hello world')
-}
+// if (userData && window.location.pathname === "/ForTesting/register.html") {
+//   window.location.assign("/ForTesting/")
+//   console.log('hello world')
+// }
 
 // console.log(userData && window.location.pathname === "/ForTesting/register.html" || window.location.pathname === '/ForTesting/register' ? window.location.assign("/ForTesting/") : console.log('userData is not exist')  )
 // console.log(userData)
