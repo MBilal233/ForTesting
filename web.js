ef7cf255cdf83cdaf837f62e5643fe8051465c90
MBilal233 <bilalameer233@gmail.com>
2023-10-09 15:21:44 +0500

    web.js and autnenticatoin.js file has been changed

--- web.js
+++ web.js
@@ -23,5 +23,5 @@ menubtn.addEventListener('click' , () =>{
     const logout = document.querySelector('.logout');
     logout.addEventListener('click',() =>{
         localStorage.clear();
-        window.location.assign('/register.html');
+        window.location.assign('/ForTesting/register.html');
     })
